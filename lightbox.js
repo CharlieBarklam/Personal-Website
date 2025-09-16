17a990bfd432dacf4aada6d5778307c7071c637b
Charlie Barklam <charles.barklam24@imperial.ac.uk>
2025-09-16 17:02:26 +0100

    Added image expansion

---
+++ lightbox.js
@@ -0,0 +1,15 @@
+(function () {
+    const lightbox = document.getElementById("lightbox");
+    const lightboxImg = document.getElementById("lightbox-img");
+
+    document.querySelectorAll("img:not(a img)").forEach(img => {
+        img.addEventListener("click", () => {
+            lightboxImg.src = img.src;
+            lightbox.classList.remove("hidden");
+        });
+    });
+
+    lightbox.addEventListener("click", () => {
+        lightbox.classList.add("hidden");
+    });
+})();
